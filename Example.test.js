4c46e0d2037eb76b3d1714cc3c1fa23fe62836d8
ppathak-19 <priyapathak.work@gmail.com>
2024-10-18 13:27:51 +0530

    example test understanding

---
+++ Example.test.js
@@ -0,0 +1,26 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
+
+// @testing-library/dom already being used here three times, whenever we access the 'screen'
+
+test("shows 6 items by default", async () => {
+  render(<App />);
+
+  const titles = await screen.findAllByRole("heading");
+  expect(titles).toHaveLength(6);
+});
+
+test("clicking on the button loads 6 more products", async () => {
+  render(<App />);
+
+  const button = await screen.findByRole("button", {
+    name: /load more/i,
+  });
+  user.click(button);
+
+  await waitFor(async () => {
+    const titles = await screen.findAllByRole("heading");
+    expect(titles).toHaveLength(12);
+  });
+});
